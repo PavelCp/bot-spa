bce574cff847272e4b62e2478b2e2e84783f409c
Pavel C <pavelcapet@gmail.com>
2024-09-17 09:16:08 +0300

    initial commit for spa

---
+++ app.js
@@ -0,0 +1,16 @@
+const tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#FF00FF";
+
+const btn = document.getElementById("btn");
+const input = document.getElementById("fname");
+
+btn.addEventListener("click", function(){
+    tg.MainButton.setText("Сообщение отправлено!");
+    tg.MainButton.show();
+    tg.sendData("sendTestMessage");
+    
+});
