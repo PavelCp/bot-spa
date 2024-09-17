d0824a521f0a80056b64276ebb60c6425ef05191
Pavel C <pavelcapet@gmail.com>
2024-09-17 12:11:05 +0300

    event click enrich003

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-const tg = window.Telegram.WebApp;
+// const tg = window.Telegram.WebApp;
 
-tg.expand();
+// tg.expand();
 
-tg.MainButton.textColor = "#FFFFFF";
-tg.MainButton.color = "#FF00FF";
+// tg.MainButton.textColor = "#FFFFFF";
+// tg.MainButton.color = "#FF00FF";
 
-const btn = document.getElementById("btn");
-const input = document.getElementById("fname");
+// const btn = document.getElementById("btn");
+// const input = document.getElementById("fname");
 
-btn.addEventListener("click", function(){
-    tg.MainButton.setText("Сообщение отправлено!");
-    tg.MainButton.show();
-    tg.sendData("sendTestMessage");
-    
-});
+// btn.addEventListener("click", function(){
+//     tg.MainButton.setText("Сообщение отправлено!");
+//     tg.MainButton.show();
+//     tg.sendData("sendTestMessage");
+// });
