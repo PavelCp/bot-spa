c7d49431d67a9ff6890be0ad09ecdf3c82bd9b1b
Pavel C <pavelcapet@gmail.com>
2024-09-17 17:28:18 +0300

    event click enrich005

--- app.js
+++ app.js
@@ -8,15 +8,17 @@ tg.MainButton.color = "#FF00FF";
 const btn = document.getElementById("btn");
 const input = document.getElementById("fname")
 const inputV = input.value;
+const inputTC = input.textContent;
 console.log('\n\ninput::', input)
 
 btn.addEventListener("click", function () {
-    tg.MainButton.setText(`Сообщение отправлено! ${input} ${inputV}`);
+    tg.MainButton.setText(`Сообщение отправлено! Input: ${input}, InputV: ${inputV}, InputTC: ${inputTC}.`);
     alert(`input::: ${input}`)
     alert(`inputV::: ${inputV}`)
     tg.MainButton.show();
     document.getElementById("inputP").textContent = inputV;
     console.log('\n\ninput::', input)
     console.log('\n\ninputV::', inputV)
+    console.log('\n\ninputTC::', inputTC)
     // tg.sendData("sendTestMessage");
 });
